a7bf8a271ba27a2d7050ee782505da1ed687c594
lu <919041098@qq.com>
2021-03-14 21:14:05 +0800

    package Icon

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,17 +1,27 @@
 <template>
   <div>
-    <router-link to="/Money">Money</router-link>
+    <router-link to="/Money">
+      <Icon name='Money'/>
+      Money
+    </router-link>
     |
-    <router-link to="/Label">Label</router-link>
+    <router-link to="/Label">
+      <Icon name="Label"/>
+      Label
+    </router-link>
     |
-    <router-link to="/Statistic">Statistic</router-link>
+    <router-link to="/Statistic">
+      <Icon name="Statistic"/>
+      Statistic
+    </router-link>
   </div>
 </template>
 
 <script lang="ts">
+
 export default {
-name: "Nav"
-}
+  name: "Nav"
+};
 </script>
 
 <style lang="scss" scoped>
