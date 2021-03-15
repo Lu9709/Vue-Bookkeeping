0d231b20a9af5aa0bae4a84050a418aaef804f2c
lu <919041098@qq.com>
2021-03-15 13:11:22 +0800

    JS组件

--- src/components/Money/Type.vue
+++ src/components/Money/Type.vue
@@ -1,15 +1,28 @@
 <template>
   <div>
     <ul class="types">
-      <li class="selected">支出</li>
-      <li>收入</li>
+      <li :class="type === '-' && 'selected'" @click="selectType('-')">支出</li>
+      <li :class="type === '+' && 'selected'" @click="selectType('+')">收入</li>
     </ul>
   </div>
 </template>
 
-<script lang="ts">
+<script>
 export default {
-name: "Type"
+  name: "Type",
+  data() {
+    return {
+      type: '-'
+    }
+  },
+  methods: {
+    selectType(type) {
+      if (type !== '-' && type !== '+') {
+        throw new Error('This Type is unknown')
+      }
+      this.type = type;
+    }
+  }
 }
 </script>
 
