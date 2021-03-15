f4ab13447020eb43bee8080061676c8210adf81b
lu <919041098@qq.com>
2021-03-15 12:28:34 +0800

    HTML和CSS改好了

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">
+  <div class="layout-wrapper" :class="classPrefix && `${classPrefix}-content`">
+    <div class="content" :class="classPrefix && `${classPrefix}-content`">
      <slot/>
     </div>
     <Nav/>
@@ -9,12 +9,13 @@
 
 <script lang="ts">
 export default {
-  name: "Layout"
+  name: "Layout",
+  props:['classPrefix']
 };
 </script>
 
 <style lang="scss" scoped>
-.nav-wrapper {
+.layout-wrapper {
   display: flex;
   flex-direction: column;
   border: 1px solid red;
