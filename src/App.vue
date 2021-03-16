36943f9f17279671b16b05b155d2024e33b2c915
lu <919041098@qq.com>
2021-03-16 19:11:20 +0800

    重新封装recordlistModel

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
 @import "~@/assets/style/reset.scss";
 @import "~@/assets/style/helper.scss";
 
-.body {
+body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #333;
