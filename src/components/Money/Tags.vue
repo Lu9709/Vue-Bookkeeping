3181cb0dd395be903bfc73a0b9b4f14f376ec507
lu <919041098@qq.com>
2021-03-17 12:42:09 +0800

    解决 Tag.vue的bug

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -4,7 +4,7 @@
       <button @click="create">新增标签</button>
     </div>
     <ul class="current">
-      <li v-for="tag in dataSource" :key="tag.id"
+      <li v-for="tag in tagList" :key="tag.id"
           :class="{selected:selectTags.indexOf(tag)>=0}"
           @click="Toggle(tag)">{{ tag.name}}
       </li>
@@ -14,12 +14,14 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import {Component, Prop} from 'vue-property-decorator';
+import {Component} from 'vue-property-decorator';
+import store from '@/store/index2';
 
 @Component
 
 export default class Tags extends Vue {
-  @Prop() readonly dataSource: string[] | undefined;
+
+  tagList = store.fetchTags();
   selectTags: string[] = [];
 
   Toggle(tag: string) {
@@ -34,10 +36,8 @@ export default class Tags extends Vue {
 
   create() {
     const name = window.prompt('请输入标签名');
-    if (name === '') {
-      window.alert('标签名不能为空');
-    } else if (this.dataSource)
-      this.$emit('update:dataSource', [...this.dataSource, name]);
+    if (!name) { return window.alert('标签名不能为空'); }
+    store.createTag(name);
   }
 
 }
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -7,7 +7,7 @@
         <FormItem field-name="备注" placeholder="在这里输入备注"
                   @update:value="onUpdateNotes"/>
       </div>
-      <Tags :data-source.sync="tags" @update:value="onTagsUpdate"/>
+      <Tags/>
     </Layout>
   </div>
 </template>
@@ -27,15 +27,10 @@ import store from '@/store/index2';
 })
 
 export default class Money extends Vue {
-  tags = store.tagList;
   recordList: RecordItem[] = store.recordList;
   record: RecordItem = {
     tags: [], notes: "", type: "-", amount: 0
   };
-
-  onTagsUpdate(value: string[]) {
-    this.record.tags = value;
-  }
 
   onUpdateNotes(value: string) {
     this.record.notes = value;
