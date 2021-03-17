ed1fb249d21bbcae557fd387798aa50092542753
lu <919041098@qq.com>
2021-03-17 20:33:59 +0800

    Money.vue内采用Tabs

--- src/views/Statistic.vue
+++ src/views/Statistic.vue
@@ -15,11 +15,10 @@
 <script lang="ts">
 import Vue from 'vue';
 import Tabs from '@/components/Tabs.vue';
-import Types from '@/components/Money/Types.vue';
 import {Component} from 'vue-property-decorator';
 
 @Component({
-  components: {Tabs, Types}
+  components: {Tabs}
 })
 export default class Statistic extends Vue {
   type = '-';
