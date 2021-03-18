d5a865e0874db9a861f12c76401dc26c00ec6fd1
lu <919041098@qq.com>
2021-03-18 14:42:44 +0800

    完成统计页面 采用dayjs

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -27,7 +27,7 @@ import {Component, Prop} from 'vue-property-decorator';
 @Component
 
 export default class NumberPad extends Vue {
-  @Prop() readonly value!: number;
+  @Prop(Number) readonly value!: number;
   output = this.value.toString();
 
   inputContent(event: MouseEvent) {
@@ -59,8 +59,9 @@ export default class NumberPad extends Vue {
   }
 
   ok() {
-    this.$emit('update:value', this.output);
-    this.$emit('submit', this.output);
+    const number = parseFloat((this.output))
+    this.$emit('update:value', number);
+    this.$emit('submit', number);
     this.output = '0';
   }
 }
--- src/views/Statistic.vue
+++ src/views/Statistic.vue
@@ -2,13 +2,11 @@
   <div>
     <Layout>
       <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
-      <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
       <ol>
-        <li v-for="(group,index) in result" :key="index">
-          <h3 class="title">{{ group.title }}</h3>
+        <li v-for="(group, index) in groupList" :key="index">
+          <h3 class="title">{{ beautify(group.title) }} <span>￥{{ group.total }}</span></h3>
           <ol>
-            <li v-for="item in group.items" :key="item.id"
-                class="record">
+            <li v-for="item in group.items" :key="item.id" class="record">
               <span>{{ tagString(item.tags) }}</span>
               <span class="notes">{{ item.notes }}</span>
               <span>￥{{ item.amount }} </span>
@@ -24,34 +22,66 @@
 import Vue from 'vue';
 import Tabs from '@/components/Tabs.vue';
 import {Component} from 'vue-property-decorator';
-import intervalList from '@/constants/intervalList';
 import recordTypeList from '@/constants/recordTypeList';
+import dayjs from 'dayjs';
+import clone from '@/lib/clone';
 
 @Component({
   components: {Tabs}
 })
 export default class Statistic extends Vue {
   type = '-';
-  interval = 'day';
   recordTypeList = recordTypeList;
-  intervalList = intervalList;
+
   tagString(tags: Tag[]) {
     return tags.length === 0 ? '无' : tags.join(',');
   }
+
   get recordList() {
     return (this.$store.state as RootState).recordList;
   }
-  get result() {
+
+  get groupList() {
     const {recordList} = this;
-    type HashTableValue = { title: string; items: RecordItem[] }
-    const hashTable: { [key: string]: HashTableValue } = {};
-    for (let i = 0; i < recordList.length; i++) {
-      const [date, time] = recordList[i].createAt!.split('T');
-      hashTable[date] = hashTable[date] || {title: date, items: []};
-      hashTable[date].items.push(recordList[i]);
+    type Result = { title: string; total?: number; items: RecordItem[] }[]
+    const newList = clone(recordList)
+        .filter(r => r.type === this.type)
+        .sort((a, b) => dayjs(b.createAt).valueOf() - dayjs(a.createAt).valueOf());
+    const result: Result = [{title: dayjs(newList[0].createAt).format('YYYY-MM-DD'), items: [newList[0]]}];
+    if (recordList.length === 0) {return [];}
+    for (let i = 1; i < newList.length; i++) {
+      const current = newList[i];
+      const last = result[result.length - 1];
+      if (dayjs(last.title).isSame(dayjs(current.createAt), 'day')) {
+        last.items.push(current);
+      } else {
+        result.push({title: dayjs(current.createAt).format('YYYY-MM-DD'), items: [current]});
+      }
     }
-    return hashTable;
+    result.map(group => {
+      group.total = group.items.reduce((sum, item) => {
+        return sum + item.amount;
+      }, 0);
+    });
+    return result;
   }
+
+  beautify(string: string) {
+    const day = dayjs(string);
+    const now = dayjs();
+    if (day.isSame(now, 'day')) {
+      return '今天';
+    } else if (day.isSame(now.subtract(1, 'day'), 'day')) {
+      return '昨天';
+    } else if (day.isSame(now.subtract(2, 'day'), 'day')) {
+      return '前天';
+    } else if (day.isSame(now, 'year')) {
+      return day.format('M月D日');
+    } else {
+      return day.format('YYYY年M月D日');
+    }
+  }
+
   beforeCreate() {
     this.$store.commit('fetchRecords');
   }
