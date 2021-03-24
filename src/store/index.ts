5ce2297abdb26d80b2cc19e2a4dcc5fc51cf2d37
lu <919041098@qq.com>
2021-03-24 11:26:30 +0800

    deloy脚本配置，添加二维码

--- src/store/index.ts
+++ src/store/index.ts
@@ -11,6 +11,7 @@ const store = new Vuex.Store({
   state: {
     recordList: [],
     createRecordError: null,
+    createTagError: null,
     tagList: [],
     currentTags: undefined
   } as RootState,
@@ -72,9 +73,11 @@ const store = new Vuex.Store({
       }
     },
     createTag(state, name: string) {
+      state.createTagError = null;
       const names = state.tagList.map(item => item.name);
       if (names.indexOf(name) >= 0) {
-        window.alert('标签名重复了');
+        state.createTagError = new Error('tag name duplicated')
+        return;
       }
       const id = createId().toString();
       state.tagList.push({id, name: name});
--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@
 const path = require('path')
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Vue-Bookkeeping-website/'
+    : '/',
   lintOnSave: false,
   chainWebpack: config => {
     const dir = path.resolve(__dirname, 'src/assets/icons')
@@ -10,10 +13,10 @@ module.exports = {
       .test(/\.svg$/)
       .include.add(dir).end() //包含icons目录
       .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract: false}).end()
-      // extract:false 不把文件解析出来
-      // svgo-loader  去除svg的fill
-      // .use('svgo-loader').loader('svgo-loader')
-      // .tap(options => ({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]})).end()
+    // extract:false 不把文件解析出来
+    // svgo-loader  去除svg的fill
+    // .use('svgo-loader').loader('svgo-loader')
+    // .tap(options => ({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]})).end()
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
     config.module.rule('svg').exclude.add(dir)
