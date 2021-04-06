6f4bce81a503367cb1b37be2a4a0ac6a46212851
lu <919041098@qq.com>
2021-04-06 15:07:18 +0800

    构造一个月的数据

--- src/views/Statistic.vue
+++ src/views/Statistic.vue
@@ -32,6 +32,8 @@ import recordTypeList from '@/constants/recordTypeList';
 import dayjs from 'dayjs';
 import clone from '@/lib/clone';
 import Chart from '@/components/Chart.vue';
+import _ from 'lodash';
+
 
 @Component({
   components: {Chart, Tabs}
@@ -100,7 +102,33 @@ export default class Statistic extends Vue {
     this.$store.commit('fetchRecords');
   }
 
+  get y() {
+    const today = new Date();
+    const array = [];
+    for (let i = 0; i <= 29; i++) {
+      const dateString = dayjs(today).subtract(i, 'day').format('YYYY-MM-DD');
+      const found = _.find(this.recordList, {
+        createAt: dateString
+      });
+      array.push({
+        date: dateString, value: found ? found.amount : 0
+      });
+    }
+    array.sort((a,b)=>{
+      if(a.date > b.date){
+        return 1;
+      }else if(a.date === b.date){
+        return 0;
+      }else {
+        return -1;
+      }
+    })
+    return array
+  }
+
   get x() {
+    const keys = this.y.map(item=>item.date)
+    const values = this.y.map(item => item.value)
     return {
       grid: {
         left: 0,
@@ -108,11 +136,7 @@ export default class Statistic extends Vue {
       },
       xAxis: {
         type: 'category',
-        data: [
-          '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
-          '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
-          '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
-        ],
+        data: keys,
         axisTick: {alignWithLabel: true},
         axisLine: {lineStyle: {color: '#666'}}
       },
@@ -124,12 +148,7 @@ export default class Statistic extends Vue {
         symbol: 'circle',
         symbolSize: 12,
         itemStyle: {borderWidth: 1, color: '#666', borderColor: '#666'},
-        data: [
-          820, 932, 901, 934, 1290, 1330, 1320,
-          820, 932, 901, 934, 1290, 1330, 1320,
-          820, 932, 901, 934, 1290, 1330, 1320,
-          820, 932, 901, 934, 1290, 1330, 1320, 1, 2
-        ],
+        data: values,
         type: 'line'
       }],
       tooltip: {
