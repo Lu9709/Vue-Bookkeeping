f4c0bcc49d8bed605496ec866cf086b0526244dc
lu <919041098@qq.com>
2021-04-06 14:43:47 +0800

    自定义Chart

--- src/views/Statistic.vue
+++ src/views/Statistic.vue
@@ -2,6 +2,7 @@
   <div>
     <Layout>
       <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
+      <Chart :options="x"/>
       <ol v-if="groupList.length>0">
         <li v-for="(group, index) in groupList" :key="index">
           <h3 class="title">{{ beautify(group.title) }} <span>￥{{ group.total }}</span></h3>
@@ -28,9 +29,10 @@ import {Component} from 'vue-property-decorator';
 import recordTypeList from '@/constants/recordTypeList';
 import dayjs from 'dayjs';
 import clone from '@/lib/clone';
+import Chart from '@/components/Chart.vue';
 
 @Component({
-  components: {Tabs}
+  components: {Chart, Tabs}
 })
 export default class Statistic extends Vue {
   type = '-';
@@ -90,10 +92,39 @@ export default class Statistic extends Vue {
   beforeCreate() {
     this.$store.commit('fetchRecords');
   }
+  get x() {
+    return {
+      xAxis: {
+        type: 'category',
+        data: [
+          '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
+          '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
+          '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
+        ]
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [{
+        data: [
+          820, 932, 901, 934, 1290, 1330, 1320,
+          820, 932, 901, 934, 1290, 1330, 1320,
+          820, 932, 901, 934, 1290, 1330, 1320,
+          820, 932, 901, 934, 1290, 1330, 1320, 1, 2
+        ],
+        type: 'line'
+      }],
+      tooltip: {show: true}
+    };
+  }
 
 }
 </script>
 <style scoped lang="scss">
+.echarts {
+  max-width: 100%;
+  height: 400px;
+}
 .noResult{
   padding: 16px;
   text-align: center;
